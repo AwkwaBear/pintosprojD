b3d603c7c803b4657c4c2f4e563f7ebdf755cba9
howtotailslide <agasbarro36@gmail.com>
2018-11-08 22:42:10 -1000

    update

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -7,28 +7,28 @@ typedef int fixed_t;
 //Define fixed point shift as 16 bits
 #define FIXPNT_SHIFT 16
 
-/* Some helpful macros. */
-/* Convert a value to fixed-point value. */
+//Macro to convert integers to fixed point values
 #define FIXPNT_CONVERT(X) ((fixed_t)(X << FIXPNT_SHIFT))
-/* Add two fixed-point value. */
+
+//macros to add two fixed point values or a fixed point and an integer
 #define FIXPNT_ADD(X,Y) (X + Y)
-/* Add a fixed-point value A and an int value B. */
 #define FIXPNT_ADD_INT(X,Y) (X + (Y << FIXPNT_SHIFT))
-/* Substract two fixed-point value. */
+
+//subtract two fixed point values or fixed point and an integer
 #define FIXPNT_SUBTRACT(X,Y) (X - Y)
-/* Substract an int value B from a fixed-point value A */
 #define FIXPNT_SUBTRACT_INT(X,Y) (X - (Y << FIXPNT_SHIFT))
-/* Multiply a fixed-point value A by an int value B. */
+
+//multiply two fixed point values or fixed point and an integer
 #define FIXPNT_MULTIPLY_INT(X,Y) (X * Y)
-/* Multiply two fixed-point value. */
 #define FIXPNT_MULTIPLY(X,Y) ((fixed_t)(((int64_t) X) * Y >> FIXPNT_SHIFT))
-/* Divide two fixed-point value. */
+
+
+//divide two fixed point values or fixed point and an integer
 #define FIXPNT_DIVIDE(X,Y) ((fixed_t)((((int64_t) X) << FIXPNT_SHIFT) / Y))
-/* Divide a fixed-point value A by an int value B. */
 #define FIXPNT_DIVIDE_INT(X,Y) (X / Y)
-/* Get integer part of a fixed-point value. */
+
+//macros to get integer from fixed point value or rounded integer
 #define FIXPNT_GET_INT(X) (X >> FIXPNT_SHIFT)
-/* Get rounded integer of a fixed-point value. */
 #define FIXPNT_ROUND(X) (X >= 0 ? ((X + (1 << (FIXPNT_SHIFT - 1))) >> FIXPNT_SHIFT) \
                             : ((X - (1 << (FIXPNT_SHIFT - 1))) >> FIXPNT_SHIFT))
 
